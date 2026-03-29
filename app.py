f92ff4c74bca0ad18c658315502f0522303c3575
mukul <contactmukulwakade@gmail.com>
2026-03-29 13:11:02 +0530

    Enable CORS for frontend

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ import json
 
 import websockets
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://crypto-price-websocket-beige.vercel.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 latest_price = {}
 MAX_CONNECTIONS = 3
