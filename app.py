ebce809c4bd84f463a2c65482e9575395c6d107d
mukul <contactmukulwakade@gmail.com>
2026-03-29 13:18:40 +0530

    Relax CORS for frontend deployment

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://crypto-price-websocket-beige.vercel.app",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
